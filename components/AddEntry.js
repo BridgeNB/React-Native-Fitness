249c52d5764b4c1fabcb197a3ffa235067abbe2b
BridgeNB <zyq123258789@gmail.com>
2018-03-17 16:55:52 -0700

    entry part done

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react'
-import { View, TouchableOpacity, Text } from 'react-native'
+import { View, TouchableOpacity, Text, StyleSheet, Platform } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
-import { getMetricMetaInfo, timeToString } from '../utils/helpers'
+import {
+  getMetricMetaInfo,
+  timeToString,
+  getDailyReminderValue
+} from '../utils/helpers'
 import FitSlider from './FitSlider'
 import Stepper from './Stepper'
 import DateHeader from './DateHeader'
 import TextButton from './TextButton'
+import { connect } from 'react-redux'
+import { addEntry } from '../actions'
+import { purple, white } from '../utils/colors'
+import { submitEntry, removeEntry } from '../utils/api'
 
 function SubmitBtn({ onPress }) {
   return (
-    <TouchableOpacity onPress={onPress}>
-      <Text>SUBMIT</Text>
+    <TouchableOpacity
+      style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn}
+      onPress={onPress}>
+        <Text style={styles.submitBtnText}>SUBMIT</Text>
     </TouchableOpacity>
   )
 }
 
-export default class AddEntry extends Component {
+class AddEntry extends Component {
   state = {
     run: 0,
     bike:0,
@@ -49,23 +59,37 @@ export default class AddEntry extends Component {
     }))
   }
   submit = () => {
-    this.setState(() => ({run: 0, bike: 0, swim: 0, sleep: 0, eat: 0}))
+    const key = timeToString()
+    const entry = this.state
+    this.props.dispatch(addEntry({
+      [key]: entry
+    }))
+    this.setState(() => ({run:0, bike:0, swim: 0, sleep: 0, eat: 0}))
+    submitEntry({key, entry})
   }
   reset = () => {
     const key = timeToString()
+
+    this.props.dispatch(addEntry({
+      [key]: getDailyReminderValue()
+    }))
+
+    // Route to Home
+
+    removeEntry(key)
   }
   render() {
     const metaInfo = getMetricMetaInfo()
 
-    if (false) {
+    if (this.props.alreadyLogged) {
       return (
-        <View>
+        <View style={styles.center}>
           <Ionicons
-            name={'ios-happy-outline'}
+            name={Platform.os === 'ios'? 'ios-happy-outline' : 'md-happy'}
             size={100}
           />
           <Text>You already logged your information for today.</Text>
-          <TextButton onPress={this.reset}>
+          <TextButton style={{padding: 10}} onPress={this.reset}>
             Reset
           </TextButton>
         </View>
@@ -73,14 +97,14 @@ export default class AddEntry extends Component {
     }
 
     return (
-      <View>
+      <View style={styles.container}>
         <DateHeader date={(new Date()).toLocaleDateString()}/>
         <Text>{JSON.stringify(this.state)}</Text>
         {Object.keys(metaInfo).map((key) => {
           const {getIcon, type, ...rest } = metaInfo[key]
           const value = this.state[key]
           return (
-            <View key={key}>
+            <View key={key} style={styles.row}>
               {getIcon()}
               {type == 'slider'
                 ? <FitSlider
@@ -103,3 +127,57 @@ export default class AddEntry extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: white
+  },
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+  },
+  iosSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40,
+  },
+  AndroidSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: 'center',
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30,
+  },
+})
+
+function mapStateToProps (state) {
+  const key = timeToString()
+  return {
+    alreadyLogged: state[key] && typeof state[key].today === 'undefined'
+  }
+}
+
+
+export default connect(mapStateToProps)(AddEntry)
